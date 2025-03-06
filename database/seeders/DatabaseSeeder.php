33120b21d132efbff9a0052feff1c1fc5c531ce9
Nurlytan <nurik.bro.2000@mail.ru>
2025-03-06 16:23:42 +0500

    rbac installation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,25 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => 'Qwerty',
+        ]);
+
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => 'Qwerty',
         ]);
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+
+        $admin = User::find(1); 
+        $admin->assignRole('admin');
+
+        $user = User::find(2); 
+        $user->assignRole('user');
+
     }
 }
